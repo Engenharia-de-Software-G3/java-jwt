a3417db0726f24335d080b413acb9b86136f2fe7
Caio-Cesar-Vieira-Cavalcanti <caio.cesar.vieira.cavalcanti@ccc.ufcg.edu.br>
2025-08-30 12:35:29 -0300

    refactor: Cognitive Complexity

--- lib/src/main/java/com/auth0/jwt/impl/PayloadSerializer.java
+++ lib/src/main/java/com/auth0/jwt/impl/PayloadSerializer.java
@@ -4,10 +4,9 @@ import com.auth0.jwt.RegisteredClaims;
 import com.fasterxml.jackson.core.JsonGenerator;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 /**
  * Jackson serializer implementation for converting into JWT Payload parts.
@@ -35,32 +34,49 @@ public class PayloadSerializer extends ClaimsSerializer<PayloadClaimsHolder> {
      * added with the {@linkplain com.auth0.jwt.JWTCreator.Builder#withPayload(Map)} method.
      */
     private void writeAudience(JsonGenerator gen, Map.Entry<String, Object> e) throws IOException {
-        if (e.getValue() instanceof String) {
-            gen.writeFieldName(e.getKey());
-            gen.writeString((String) e.getValue());
-        } else {
-            List<String> audArray = new ArrayList<>();
-            if (e.getValue() instanceof String[]) {
-                audArray = Arrays.asList((String[]) e.getValue());
-            } else if (e.getValue() instanceof List) {
-                List<?> audList = (List<?>) e.getValue();
-                for (Object aud : audList) {
-                    if (aud instanceof String) {
-                        audArray.add((String) aud);
-                    }
-                }
-            }
-            if (audArray.size() == 1) {
-                gen.writeFieldName(e.getKey());
-                gen.writeString(audArray.get(0));
-            } else if (audArray.size() > 1) {
-                gen.writeFieldName(e.getKey());
-                gen.writeStartArray();
-                for (String aud : audArray) {
-                    gen.writeString(aud);
-                }
-                gen.writeEndArray();
-            }
+        Object value = e.getValue();
+        if (value == null) {
+            return;
         }
+
+        if (value instanceof String) {
+            gen.writeFieldName(e.getKey());
+            gen.writeString((String) value);
+            return;
+        }
+
+        List<String> aud = extractAudienceStrings(value);
+
+        if (aud.isEmpty()) {
+            return;
+        }
+
+        gen.writeFieldName(e.getKey());
+        if (aud.size() == 1) {
+            gen.writeString(aud.get(0));
+            return;
+        }
+
+        gen.writeStartArray();
+        for (String s : aud) {
+            gen.writeString(s);
+        }
+        gen.writeEndArray();
+    }
+
+    private List<String> extractAudienceStrings(Object value) {
+        if (value instanceof String[]) {
+            return Arrays.stream((String[]) value)
+                    .filter(Objects::nonNull)
+                    .collect(Collectors.toList());
+        }
+
+        if (value instanceof Iterable<?>) {
+            return StreamSupport.stream(((Iterable<?>) value).spliterator(), false)
+                    .filter(String.class::isInstance)
+                    .map(String.class::cast)
+                    .collect(Collectors.toList());
+        }
+        return Collections.emptyList();
     }
 }
